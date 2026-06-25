5738a93bbd713973f650bfea198382bd572f8cd7
Jon-Martin Storm <jonmartin.storm@gmail.com>
2026-06-25 13:26:01 +0200

    strings and benchmarks

---
+++ iteration/iteration_test.go
@@ -0,0 +1,24 @@
+package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected '%q' but got '%q'", expected, repeated)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("b", 3)
+	println(repeated)
+	// Output: bbb
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
