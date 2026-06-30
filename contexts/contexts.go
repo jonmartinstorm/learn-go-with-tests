958f6d04723ffc5ee101171bb286833fc3f4b801
Jon-Martin Storm <jonmartin.storm@gmail.com>
2026-06-30 10:21:31 +0200

    context.Context

---
+++ contexts/contexts.go
@@ -0,0 +1,23 @@
+package contexts
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			//fmt.Printf("%q", err)
+			return
+		}
+		fmt.Fprint(w, data)
+	}
+}
